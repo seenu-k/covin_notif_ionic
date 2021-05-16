e7769fd3e6a6c1efa4d58f1a9541d7bae958caa3
Vanta Pretos <inbox.seenu@gmail.com>
2021-05-17 01:32:36 +0530

    Add getStates and getDistricts in dataService

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import '@codetrix-studio/capacitor-google-auth';
 import { Plugins } from '@capacitor/core';
@@ -6,14 +10,23 @@ import { Plugins } from '@capacitor/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
 
+import { StatesResponse, State, DistrictResponse } from './dataModel';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  states = new BehaviorSubject<State[]>([]);
+
   constructor(
-    public auth: AngularFireAuth
-  ) { }
+    public auth: AngularFireAuth,
+    private http: HttpClient
+  ) {
+    this.getStates().subscribe(states => {
+      this.states.next(states);
+    });
+  }
 
   async login() {
     const googleUser = await Plugins.GoogleAuth.signIn();
@@ -23,6 +36,17 @@ export class DataService {
 
   logout() {
     return this.auth.signOut();
+  }
+
+  getStates() {
+    return this.http.get<StatesResponse>('https://cdn-api.co-vin.in/api/v2/admin/location/states')
+      .pipe(map(stateResponse => stateResponse.states));
+  }
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  getDistricts(state_id: number) {
+    return this.http.get<DistrictResponse>(`https://cdn-api.co-vin.in/api/v2/admin/location/districts/${state_id}`)
+      .pipe(map(districtResponse => districtResponse.districts));
   }
 
 }
